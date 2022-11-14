ef09fcf371514eab1a96930a066318d049ec91df
Deepag kumar B <deepagkumar@gmail.com>
2022-11-14 23:14:07 +0530

    Completed animation of sprites

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,104 @@ playerImage.src = 'images/shadow_dog.png';
 var spriteWidth = 575;//=6876/12 cols approx
 var spriteHeight = 523;//=5230/10 rows approx
 
+
+const spriteImageActions = [
+    {
+        name: 'Idle',
+        row: 0,
+        columns: 6
+
+    },
+    {
+        name: 'Jump',
+        row: 1,
+        columns: 6
+
+    },
+    {
+        name: 'Fall',
+        row: 2,
+        columns: 6
+
+    },
+    {
+        name: 'Run',
+        row: 3,
+        columns: 8
+    },
+    {
+        name: 'Dizzy',
+        row: 4,
+        columns: 10
+    },
+    {
+        name: 'Sit',
+        row: 5,
+        columns: 4
+    },
+    {
+        name: 'Roll',
+        row: 6,
+        columns: 6
+    },
+    {
+        name: 'Attack',
+        row: 7,
+        columns: 6
+    },
+    {
+        name: 'KO',
+        row: 8,
+        columns: 11
+    },
+    {
+        name: 'Howl',
+        row: 9,
+        columns: 3
+    }
+];
+
+var animateAction = 'Idle';
+var noOfColumns = spriteImageActions.find(action => action.name === animateAction).columns;
+
+var xPosition = 0;
+var yPosition = spriteImageActions.find(action => action.name === animateAction).row;
+var framesPerSecond = 40;
+
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.fillRect(100, 50, 100, 100);
     //ctx.drawImage(playerImage, sx, sy, sw, sh, dx, dy, dw, dh);
-    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    //var position = Math.floor(xPosition/spriteRefreshDelay)%6;
+    ctx.drawImage(playerImage, xPosition * spriteWidth, yPosition * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
-    requestAnimationFrame(animate);
+    if(xPosition < noOfColumns){
+        xPosition++;
+    }else{
+        xPosition = 0;
+    }
+
+    setTimeout(function(){
+        requestAnimationFrame(animate);
+    },6000/framesPerSecond);
 }
 
+function fillActionsDropdown(){
+    var actionDropDown = document.getElementById('action');
+    spriteImageActions.forEach((action)=>{
+        var opt = document.createElement('option');
+        opt.value = action.name;
+        opt.innerHTML = action.name;
+        actionDropDown.appendChild(opt);
+    });
+}
+
+
+document.getElementById('action').onchange = function() {
+    animateAction = document.getElementById('action').value;
+    noOfColumns = spriteImageActions.find(action => action.name === animateAction).columns;
+    yPosition = spriteImageActions.find(action => action.name === animateAction).row;
+};
+
+fillActionsDropdown();
 animate();
